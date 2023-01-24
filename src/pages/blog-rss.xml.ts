4058f5ab9cefc99d111c5eb870f410ce7eb5adbd
Isaiah A <isaiah.althouse@gmail.com>
2023-01-24 15:33:51 -0700

    Fix RSS for Astro 2.0

--- src/pages/blog-rss.xml.ts
+++ src/pages/blog-rss.xml.ts
@@ -1,21 +1,21 @@
 import rss from '@astrojs/rss';
-import { slugify } from '../utils/slugify';
+import { getCollection } from 'astro:content';
 
 // TODO: include full post content
 // https://docs.astro.build/en/guides/rss/#including-full-post-content
 
-const postImportResult = import.meta.glob('../content/blog/**/*.md', { eager: true });
+const postImportResult = await getCollection("blog");
 const posts = Object.values(postImportResult);
 
 export const get = () => rss({
   title: "Isaiah's Blog",
   description: 'Programming foibles',
   site: import.meta.env.SITE,
-  items: posts.map(({ frontmatter }: any) => ({
-    link: `/blog/${slugify(frontmatter)}`,
-    title: frontmatter.title,
-    description: frontmatter.description,
-    pubDate: frontmatter.publishedAt,
+  items: posts.map(({ data, slug }) => ({
+    link: `/blog/${slug}`,
+    title: data.title,
+    description: data.description,
+    pubDate: data.publishedAt,
   })),
   customData: `<language>en-us</language>`,
 });
